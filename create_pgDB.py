ae3a70cb275dee966f319bdd1746ce8d836ec91c
mhsjacoby <margarite.jacoby@colorado.edu>
2020-11-12 15:17:38 -0800

    add functionality to create a probability database

--- create_pgDB.py
+++ create_pgDB.py
@@ -21,23 +21,18 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 from my_functions import *
-from pg_functions import *
 from write_occupancy import *
-
-# import prepare_data_for_DB as db_import
-
-
+from pg_functions import *
 
 img_ffill_limit = 1
 img_bfill_limit = 1
 
 
 def read_data():
-    inf_df = prepare_data_for_DB(root_dir)
-    summarize_df(inf_df)
-    filled_df = fill_df(inf_df)
-    filled_df.fillna(value=0, inplace=True)
-    summarize_df(filled_df, name='0_fill')
+    df = prepare_data_for_DB(root_dir, db_type=db_type)
+    # summarize_df(df)
+    filled_df = fill_df(df)
+    filled_df.fillna(value=1, inplace=True)
     return filled_df
 
 def summarize_df(df, name='before_fill'):
@@ -60,19 +55,22 @@ def fill_df(df):
 
 
 
-def create_pg(df, drop=False):
-    table_name = f'{home_parameters["home"]}_inf'
+def create_pg(df, db_type, drop=False):
+    table_name = f'{home_parameters["home"]}_{db_type}'
 
     if drop:
         pg.drop_table(table_name)
 
-    pg.create_table(table_name=table_name)
+    if db_type == 'inf':
+        pg.create_inf_table(table_name=table_name)
+
+    if db_type == 'prob':
+        pg.create_prob_table(table_name=table_name)
+
     pg.insert_table(df=df, table=table_name)
 
 
-
-
-def prepare_data_for_DB(path, save_loc=''):
+def prepare_data_for_DB(path, db_type, save_loc=''):
     home_system = os.path.basename(path.strip('/'))
     H_num, color = home_system.split('-')
 
@@ -103,14 +101,26 @@ def prepare_data_for_DB(path, save_loc=''):
             day_dfs = []
             
             for day in dates:
-                # print(os.path.basename(day).strip('.csv'))
-                day_dfs.append(pd.read_csv(day, index_col='timestamp', usecols=['timestamp', 'occupied']))
-                
+                if db_type == 'prob':
+                    f_df = pd.read_csv(day, index_col='timestamp')       
+                    cols = [col for col in f_df.columns if 'prob' in col]
+                    f_df = f_df[cols]
+                    day_dfs.append(f_df)
+
+                if db_type == 'inf':
+                    day_dfs.append(pd.read_csv(day, index_col='timestamp', usecols=['timestamp', 'occupied']))
+
+
             mod_df = pd.concat(day_dfs)
-            mod_df.columns = [mod.split('_')[0]]
+            if mod == 'env_inf' and db_type == 'prob':
+                cols = [col.split('_')[1] for col in mod_df.columns]
+                mod_df.columns = cols
+            else: 
+                mod_df.columns = [mod.split('_')[0]]
+
             all_mods.append(mod_df)
         all_mods.append(occupancy_df)
-        
+
         for df in all_mods:
             df.index = pd.to_datetime(df.index)
             df['hub'] = hub
@@ -118,11 +128,12 @@ def prepare_data_for_DB(path, save_loc=''):
 
         hub_df = pd.concat(all_mods, axis=1, sort=False)
         hub_df.drop(hub_df.tail(1).index,inplace=True) 
-        hub_df.to_csv(os.path.join(save_path, f'{H_num}_{hub}_inf.csv'))    
+        hub_df.to_csv(os.path.join(save_path, f'{H_num}_{hub}_{db_type}.csv'))    
         all_hubs.append(hub_df) 
         
     df = pd.concat(all_hubs)
-    
+    print(df)
+
     df['date'] = df.index.get_level_values(0)
     df.insert(loc=0, column='day', value=df['date'].dt.date)
     df.insert(loc=1, column='hr_min_sec', value=df['date'].dt.time)
@@ -134,7 +145,7 @@ def prepare_data_for_DB(path, save_loc=''):
 
     df.insert(loc=0, column='entry_id', value = df.index+1)
 
-    df.to_csv(os.path.join(save_path, f'{home_system}_full_inf.csv'))
+    df.to_csv(os.path.join(save_path, f'{home_system}_full_{db_type}.csv'))
     
     return(df)
 
@@ -145,33 +156,34 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Description")
 
     parser.add_argument('-path','--path', default='', type=str, help='path of stored data') # Stop at house level, example G:\H6-black\
+    parser.add_argument('-db_type','--db_type', default='inf', type=str, help='Type of database to create (inference, probability, ...')
     args = parser.parse_args()
     root_dir = args.path
+    db_type = args.db_type
 
     home_system = os.path.basename(root_dir.strip('/'))
-    # H_num, color = home_system.split('-')
 
     occ_file = os.path.join(root_dir, 'Inference_DB', 'Full_inferences', f'{home_system}_occupancy.csv')
     if not os.path.isfile(occ_file):
         print('No occupancy summary found. Generating CSV...')
         write_occupancy_df(root_dir)
     
-    inf_file = os.path.join(root_dir, 'Inference_DB', 'Full_inferences', f'{home_system}_full_inf.csv')
-    if not os.path.isfile(inf_file):
-        print('Full inference not found. Generating inference CSV....')
+    db_file = os.path.join(root_dir, 'Inference_DB', 'Full_inferences', f'{home_system}_full_{db_type}.csv')
+    if not os.path.isfile(db_file):
+        print(f'Full inference not found. Generating CSV for: {db_file}  ....')
         final_df = read_data()
     else:
-        print(f'Reading df from: {inf_file}')
-        final_df = pd.read_csv(inf_file)
+        print(f'Reading df from: {db_file}')
+        final_df = pd.read_csv(db_file)
         final_df.drop(columns=['Unnamed: 0'], inplace=True)
-        summarize_df(final_df, name='after_read')
-        
 
-    for col in ['img', 'audio', 'env', 'occupied']:
-        final_df[col] = final_df[col].fillna(0.0).astype(int)
+    not_fill_cols = ['entry_id', 'day', 'hr_min_sec', 'hub', 'occupied']
+    cols = [col for col in final_df.columns if col not in not_fill_cols]
+    for col in cols:
+        final_df[col] = final_df[col].fillna(1.0).astype(int)
+    
 
-    # home_parameters = {'home': f'{H_num.lower()}_{color}'}
     home_parameters = {'home': home_system.lower().replace('-', '_')}
     pg = PostgreSQL(home_parameters)
-    create_pg(final_df, drop=True)
+    create_pg(final_df, db_type, drop=True)
 
--- pg_functions.py
+++ pg_functions.py
@@ -39,23 +39,35 @@ class Structs():
                 occupied integer NOT NULL
             )
             """
+
+        self.probability_table = """
+            CREATE TABLE curr_schema.tab (
+                entry_id integer PRIMARY KEY,
+                day date NOT NULL,
+                hr_min_sec time without time zone NOT NULL,
+                hub character(3) NOT NULL,
+                img real,
+                audio real,
+                temp real,
+                rh real,
+                light real,
+                co2eq real,
+                occupied integer NOT NULL
+            )
+            """
         
         self.drop = """
             DROP TABLE curr_schema.tab ;
             """
-        
-        # self.select_query = """
-        #     SELECT hr_min_sec, hub, audio, env, occupancy
-        #     FROM h2_red_inference
-        #     """
 
 
 class PostgreSQL(Structs):
     def __init__(self, home_parameters, connection_params=connection_parameters,):
         self.P = connection_params
-        self.home = home_parameters['home']
+        self.home = home_parameters["home"]
         Structs.__init__(self)
-        self.inf_table=None
+        # self.inf_table=None
+        # self.prob_table=None
 
 
     def connect(self):
@@ -63,12 +75,12 @@ class PostgreSQL(Structs):
         
         try:
             conn = psycopg2.connect(
-                host=self.P['host'], 
-                database=self.P['db'], 
-                user=self.P['usr'], 
-                password=self.P['pw'])
+                host=self.P["host"], 
+                database=self.P["db"], 
+                user=self.P["usr"], 
+                password=self.P["pw"])
         except (Exception, psycopg2.DatabaseError) as error:
-            print(f'error with connect: {error}')
+            print(f'Error with connect: {error}')
 
         print(f'\n>Connecting to database: {self.P["db"]}')
         return conn
@@ -84,7 +96,7 @@ class PostgreSQL(Structs):
             print(sucess_statement) 
 
         except (Exception, psycopg2.DatabaseError) as error:
-            print(f'Error with connection: {error}')
+            print(f'Error with PG_connect: {error}')
 
         finally:
             if conn is not None:
@@ -92,27 +104,39 @@ class PostgreSQL(Structs):
                 print('>Database connection closed.')  
 
 
-    def create_table(self, schema='public', table_name=None):
-        # table_name = self.home + '_inference' if not t_name else self.home + t_name
-        print(f'Creating table: {table_name}')
-        self.inf_table = f'{schema}.{table_name}'
+    # def create_inf_table(self, table_name=None, schema="public"):
+    def create_inf_table(self, table_name=None, schema="test"):
+
+        print(f'Creating inference table: {table_name}')
+        # self.inf_table = f'{schema}.{table_name}'        
         ex = self.inference_table.replace("tab", table_name).replace("curr_schema", schema)
         self.PG_connect(ex, f'Table {table_name} created sucessfully!')
+
+    def create_prob_table(self, table_name=None, schema="test"):
+    # def create_prob_table(self, table_name=None, schema="public"):
+        print(f'Creating probability table: {table_name}')
+        # self.prob_table = f'{schema}.{table_name}'        
+        ex = self.probability_table.replace("tab", table_name).replace("curr_schema", schema)
+        self.PG_connect(ex, f'Table {table_name} created sucessfully!')
+
         
-        
-    def drop_table(self, table_name, schema="public"):
+    # def drop_table(self, table_name, schema="public"):
+    def drop_table(self, table_name, schema="test"):
+
         print(f'Dropping table: {table_name}.')
         ex = self.drop.replace("tab", table_name).replace("curr_schema", schema)
         self.PG_connect(ex, f'Table {table_name} sucessfully dropped.')
     
     
-    def insert_table(self, df, table=None, schema="public"):
+    # def insert_table(self, df, table, schema="public"):
+    def insert_table(self, df, table, schema="test"):
+
         conn = self.connect()
-        table = self.inf_table if not table else f'{schema}.{table}'
+        table = f'{schema}.{table}'
         print(table)
         try:
             tuples = [tuple(x) for x in df.to_numpy()]
-            cols = ','.join(list(df.columns))
+            cols = ",".join(list(df.columns))
             query = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
             cur = conn.cursor()
             try:
@@ -127,7 +151,7 @@ class PostgreSQL(Structs):
             cur.close()
 
         except (Exception, psycopg2.DatabaseError) as error:
-            print(f'Error with connection: {error}')
+            print(f'Error with insert_table: {error}')
         
         finally:
             if conn is not None:
